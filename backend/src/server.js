d8d85f05bbd56a40284d59cd6c4ddcb8238f2333
Kevin Vitto <kevin@Kevins-MacBook-Air.local>
2026-05-07 12:32:39 +0700

    feat: add server.js for running the api

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.example" });
+
+const { default: app } = await import("./app.js");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
